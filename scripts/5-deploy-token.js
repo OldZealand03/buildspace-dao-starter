7bfc88c2ca95486605b3386bdabcedf5eea0b2f5
OldZealand03 <isaacwallacesmith@gmail.com>
2022-04-13 23:21:28 -0600

    ERC20, ENS, and meme coin capability

---
+++ scripts/5-deploy-token.js
@@ -0,0 +1,24 @@
+import { AddressZero } from "@ethersproject/constants";
+import sdk from "./1-initialize-sdk.js";
+
+(async () => {
+  try {
+    // Deploy a standard ERC-20 contract.
+    const tokenAddress = await sdk.deployer.deployToken({
+      // What's your token's name? Ex. "Ethereum"
+      name: "DZ DAO Governance Token",
+      // What's your token's symbol? Ex. "ETH"
+      symbol: "DZ Coin",
+      // This will be in case we want to sell our token,
+      // because we don't, we set it to AddressZero again.
+      primary_sale_recipient: AddressZero,
+    });
+    console.log(
+      "✅ Successfully deployed token module, address:", // 0x85456Dc39A69B8682cf6eC29082cDE391EfA305d
+      tokenAddress,
+    );
+  } catch (error) {
+    console.error("failed to deploy token module", error);
+  }
+})();
+
--- src/App.jsx
+++ src/App.jsx
@@ -69,7 +69,7 @@ const App = () => {
   if (!address) {
     return (
       <div className="landing">
-        <h1>Welcome to NarutoDAO</h1>
+        <h1>Welcome to DZ DAO</h1>
         <button onClick={connectWithMetamask} className="btn-hero">
           Connect your wallet
         </button>
